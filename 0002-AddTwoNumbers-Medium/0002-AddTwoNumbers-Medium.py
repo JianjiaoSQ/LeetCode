4c08c04076feffa7a6a5f5f0979fd35eff56a442
JianjiaoSQ <1228196629@qq.com>
2025-07-06 23:40:48 +0800

    Time: 7 ms (32.18%), Space: 17.9 MB (35.67%) - LeetHub

--- 0002-AddTwoNumbers-Medium/0002-AddTwoNumbers-Medium.py
+++ 0002-AddTwoNumbers-Medium/0002-AddTwoNumbers-Medium.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode], carry=0) -> Optional[ListNode]:
+        # 这个题是按从小到大排列的，如果是从大到小排列尼？那就先把两个链表进行翻转，再进行下面的处理
         if not l1 and not l2 and carry == 0:
             return None
         
