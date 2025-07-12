e1499c24a32cb41db5fbb7262d096006c6d8512d
JianjiaoSQ <1228196629@qq.com>
2025-07-12 15:13:57 +0800

    Time: 37 ms (43.29%), Space: 34.8 MB (83.71%) - LeetHub

--- 0234-PalindromeLinkedList-Easy/0234-PalindromeLinkedList-Easy.py
+++ 0234-PalindromeLinkedList-Easy/0234-PalindromeLinkedList-Easy.py
@@ -5,30 +5,35 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
         middle = self.middleNode(head)
-        new = self.reverseLinkedList(middle)
+        new = self.reverse(middle)
+
         while new:
             if new.val != head.val:
                 return False
             new = new.next
             head = head.next
         return True
-    
-    # 206. 反转链表
-    def reverseLinkedList(self, head):
-        pre, cur = None, head
-        while cur:
-            tmp = cur.next
-            cur.next = pre
-            pre = cur
-            cur = tmp
-        return pre
-            
-    # 876. 链表的中间结点
+
     def middleNode(self, head):
         fast = slow = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
         return slow
+        
+    def reverse(self, head):
+        pre = None
+        cur = head
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
+
+
+        
+
         
